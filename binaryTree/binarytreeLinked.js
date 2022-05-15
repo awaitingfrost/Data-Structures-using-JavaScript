1ec2645dd66ae9cef812ac212c3bc9747ca79100
sagar subedi <sagar.subedi.1884@gmail.com>
2022-05-15 14:31:22 +0545

    binarySearch

--- binaryTree/binarytreeLinked.js
+++ binaryTree/binarytreeLinked.js
@@ -33,6 +33,23 @@ class BinarySearchTree {
       }
     }
   }
+
+  lookup(value) {
+    if (!this.root) {
+      return false;
+    }
+    let currentNode = this.root;
+    while (currentNode) {
+      if (value < currentNode.value) {
+        currentNode = currentNode.left;
+      } else if (value > currentNode.value) {
+        currentNode = currentNode.right;
+      } else if (currentNode.value === value) {
+        return currentNode;
+      }
+    }
+    return false;
+  }
 }
 
 const tree = new BinarySearchTree();
@@ -44,10 +61,13 @@ tree.insert(58);
 tree.insert(2);
 tree.insert(18);
 
+tree.lookup(45); // Node{left:null,right:null,value:45}
+tree.lookup(85); // false;
+
 //               9
 //       4                23
 //   2       45      18          58
 //
-
+// dive and conquer algo
 // Time complexity O(log n)
 // Space complexity O(n)
